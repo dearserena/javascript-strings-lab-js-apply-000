5d6d0b396dcc88c8a26ceab5055ce3737787680a
serena Lee <dearserenalee@gmail.com>
2019-08-19 20:44:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`;
+=======
+var conversation = `Let's talk about ${topic}.`;
+>>>>>>> 26e18db5ee317a2922af5af1fd969bfc0942c5f9
